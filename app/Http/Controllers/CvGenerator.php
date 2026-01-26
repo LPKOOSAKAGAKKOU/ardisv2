3ccc761fc3dd69c61eae8b2da0aa38929c36abe6
ADIP21 <prasetyoadip148@gmail.com>
2026-01-26 13:27:49 +0700

    menambah modul cv v2

--- app/Http/Controllers/CvGenerator.php
+++ app/Http/Controllers/CvGenerator.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\InterviewDetail; // Pastikan import model ini
 use App\Services\YunervaService;
